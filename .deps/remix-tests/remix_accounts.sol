94ede8fbaed99abe9baa20db0ced7a7f32f36883
a780931 <a7809831@163.com>
2025-08-21 18:48:34 +0800

    update

--- .deps/remix-tests/remix_accounts.sol
+++ .deps/remix-tests/remix_accounts.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 library TestsAccounts {
-    function getAccount(uint index) pure public returns (address) {
+    function getAccount(uint index)  public pure  returns (address) {
         address[15] memory accounts;
 		accounts[0] = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
